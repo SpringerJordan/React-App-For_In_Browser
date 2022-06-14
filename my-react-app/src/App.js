552b4f94174efe1e898e730d7c282fcaa3e3d0ef
SpringerJordan <jordantaylor9.21@gmail.com>
2022-06-14 17:27:28 +0000

    first commit

---
+++ my-react-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+//import BasicGrid from './components/PageLayout';
+import PageLayout from "./components/PageLayout";
+
+function App() {
+  return (
+    <div>
+      <PageLayout/>
+      </div>
+  );
+} 
+export default App;
